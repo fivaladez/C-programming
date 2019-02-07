2fa0b3915a04262c64c66698b31d0498441cc654
Ivan Valadez <ivan_valadez_11@hotmail.com>
2019-02-06 23:25:15 -0600

    Se añanido Exercice.c y se modifico Pointers.c

---
+++ Exercice01.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MESSAGE_TO(a,b) \
+    printf("    Hello " #a "and " #b ", have a great day!!\n")
+
+#define MAX_NUM(x,y) ( x>y ? x : y)
+
+typedef struct students{
+    char name[50];
+    int age;
+    float grade;
+
+}student;
+
+void initStruct( student * myStruct);
+void getInfo( student * myStruct);
+
+void main(){
+
+    student Ivan;
+    student Aaron;
+    initStruct( &Ivan);
+    initStruct( &Aaron);
+
+    getInfo( &Ivan );
+    getInfo( &Aaron );
+
+    printf("%s\n", Ivan.name);
+    MESSAGE_TO( Ivan , Aaron );
+    printf("    The max grade between Aaron and Ivan is: %.2f", MAX_NUM( Ivan.grade, Aaron.grade ));
+
+}
+
+void initStruct( student * myStruct){
+
+    strcpy(myStruct->name, "Francisco Valadez");
+    myStruct->age = 12;
+    myStruct->grade = 9.0;
+
+    printf("    The default name is: %s\n", myStruct->name);
+    printf("    The default age is: %d\n", myStruct->age);
+    printf("    The default grade is: %d\n", myStruct->grade);
+    printf("    =====   \n\n");
+}
+
+void getInfo( student * myStruct){
+    printf("\n    Intoduce your first name: ");
+    scanf("%s", myStruct->name);
+    char temporaryName[50];
+    printf("\n    Intoduce your last name: ");
+    scanf("%s", temporaryName);
+    strcat( myStruct->name, " ");
+    strcat( myStruct->name, temporaryName);
+    printf("    Hello  %s\n", myStruct->name );
+
+    printf("\n    Intoduce your age: ");
+    scanf("%i", &myStruct->age);
+    printf("    Your age is %i\n", myStruct->age );
+
+    printf("\n    Intoduce your grade: ");
+    scanf("%f", &myStruct->grade);
+    printf("    Your grade is %.2f\n", myStruct->grade );
+}
--- Pointers.c
+++ Pointers.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 void main(){
+
     int  var = 20;   /* actual variable declaration */
     int  *ip;        /* pointer variable declaration */
-
     ip = &var;  /* store address of var in pointer variable*/
     printf("Address of var variable: %x\n", &var  );
     /* address stored in pointer variable */
     printf("Address stored in ip variable: %x\n", ip );
     /* access the value using the pointer */
-    printf("Value of *ip variable: %d\n", *ip );
+    printf("Value of *ip variable: %d\n\n", *ip );
+
     /* Null pointers*/
     int  *ptr = NULL;
-    printf("The value of ptr is : %x\n", ptr  );
+    printf("The value of ptr is : %x\n\n", ptr  );
     /*Check for a NULL pointer*/
     /*
         - if(ptr)     succeeds if p is not null
@@ -25,28 +26,30 @@ void main(){
     ptr2 = var2;
     for ( i = 0; i < 3; i++) {
        printf("Address of var[%d] = %x\n", i, ptr2 );
-       printf("Value of var[%d] = %d\n", i, *ptr2 );
+       printf("Value of var[%d] = %d\n", i, *(ptr2 + i) );
        /* move to the next location */
-       ptr2++;
+       //ptr2++;
     }
-    for ( i = 0; i < MAX; i++) {
-       ptr[i] = &var[i]; /* assign the address of integer. */
+    for ( i = 0; i < 3; i++) {
+       ptr3[i] = &var2[i]; /* assign the address of integer. */
     }
-    for ( i = 0; i < MAX; i++) {
-       printf("Value of var[%d] = %d\n", i, *ptr[i] );
+    for ( i = 0; i < 3; i++) {
+       printf("Value of var[%d] = %d\n", i, *ptr3[i] );
     }
+    printf("    =====   \n\n");
+
 
     int **pptr, *ptr4;
     int var3 = 3000;
     /* take the address of var */
-    ptr = &var;
+    ptr4 = &var3;
 
     /* take the address of ptr using address of operator & */
-   pptr = &ptr;
+   pptr = &ptr4;
 
     /* take the value using pptr */
-    printf("Value of var = %d\n", var );
-    printf("Value available at *ptr = %d\n", *ptr );
+    printf("Value of var = %d\n", var3 );
+    printf("Value available at *ptr = %d\n", *ptr4 );
     printf("Value available at **pptr = %d\n", **pptr);
 
     /* To see more info about pointers, see Arrays.c file */
